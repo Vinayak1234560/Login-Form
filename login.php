a7c49fc353f84bd8a4a117102ae75f61dc90b970
Vinayak1234560 <58649178+Vinayak1234560@users.noreply.github.com>
2019-12-17 21:42:29 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <link rel="stylesheet" href="styles.css">
+<title>	Login </title>
+</head>
+<body>
+    <div class="first-container">
+        <img class="background-image" src="bg-desktop.svg" alt="">
+
+        <div class="form-container">
+            <form class="form" action="register.php">
+                <table border="1" style="margin: 0px auto;">
+                <tr> <td colspan="2" style="text-align: center; font-weight: bold; height:30px;">Login :</td>
+                </tr>
+                <tr>
+                <td style="text-align: center">Username :</td>
+                <td><input type="text" name="username" style="height: 22px; width: 225px"></td>
+                </tr>
+                <td>Password :</td>
+                <td><input type="text" name="passwod" style="height: 22px; width: 225px"></td>
+                
+                </tr>
+                    <td style="text-align: center"><input type="submit" value="Login   " style="margin: 0px auto;height: 30px; font-weight: bold;"></td>
+                    <td style="text-align: center"><input type="submit" value="Register" style="height: 30px; font-weight: bold;"></td>
+                </tr>
+                </table>
+                </form>
+        </div>
+    </div>
+  
+</body>
+</html>
